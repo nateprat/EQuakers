5ffa9dd576f1c0dc583eb744a9e76cdde084c3bb
voide <nathan.praties@gmail.com>
2021-03-11 22:28:10 +0000

    rename

--- app/src/androidTest/java/com/nateprat/mobileplatformdevelopment/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nateprat/mobileplatformdevelopment/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.nateprat.equakers", appContext.getPackageName());
+        assertEquals("com.nateprat.university.mobileplatformdevelopment", appContext.getPackageName());
     }
 }
